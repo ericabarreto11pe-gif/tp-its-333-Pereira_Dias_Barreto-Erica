10f47f3af4993436bb4e2ecbbda5471023a8e267
Pereira Erica <ericabarreto11.pe@gmail.com>
2026-01-12 18:01:20 +0100

    Remise des exercices

--- srcCodeAPIs/mySecondAPI_with_MVC/app/views.py
+++ srcCodeAPIs/mySecondAPI_with_MVC/app/views.py
@@ -2,9 +2,38 @@ from app import app
 from flask import render_template, request, jsonify
 
 ### EXO1 - simple API
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+### EXO3 - API with parameters display 
+@app.route('/api/utilisateur', methods=['GET'])
+def utilisateur():
+    # données FIXES dans le backend
+    nom = "Erica"
+    age = 23
+
+    return jsonify(
+        nom=nom,
+        age=age
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
 
 ### EXO2 - API with simple display
 
-### EXO3 - API with parameters display 
 
 ### EXO4 - API with parameters retrieved from URL 
+
+@app.route('/api/utilisateur/<nom>/<int:age>', methods=['GET'])
+def utilisateur(nom, age):
+    return jsonify(
+        nom=nom,
+        age=age
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True)
