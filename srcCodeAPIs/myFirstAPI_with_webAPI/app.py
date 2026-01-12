692fcc1c204f9cc8fe745d3a11a54c0743c1da8d
Pereira Erica <ericabarreto11.pe@gmail.com>
2026-01-12 16:28:59 +0100

    Premier commit

--- srcCodeAPIs/myFirstAPI_with_webAPI/app.py
+++ srcCodeAPIs/myFirstAPI_with_webAPI/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+#from flask import Flask, jsonify, request
+
+#app = Flask(__name__)
 
 ## EXO1: API GET: renvoyer un helloworld - API end point name: "api/salutation"
 
@@ -8,7 +11,32 @@ app = Flask(__name__)
 
 # to be tested with curl: 
 # >> curl -i -X GET http://localhost:5000/api/salutation
-# >> curl -i -X POST -H 'Content-Type: application/json' -d '{"nom": "Bob"}' http://localhost:5000/api/utilisateurs
+ #>> curl -i -X POST -H 'Content-Type: application/json' -d '{"nom": "Bob"}' http://localhost:5000/api/utilisateurs
+
+#if __name__ == '__main__':
+#    app.run(debug=True)
+
+#from flask import Flask, jsonify
+
+@app.route('/api/salutation', methods=['GET'])
+
+def salutation():
+    return jsonify(message="Hello World")
 
 if __name__ == '__main__':
     app.run(debug=True)
+@app.route('/api/utilisateurs', methods=['POST'])
+def creer_utilisateur():
+    data = request.get_json()      # récupérer le JSON envoyé
+    nom = data.get('nom')          # extraire le paramètre "nom"
+
+    return jsonify(
+        message="Utilisateur reçu",
+        nom=nom
+    )
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+
+
